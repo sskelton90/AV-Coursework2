ad84f9f23f03ea6e4c0bb64226f3af8116291ab7
Wenqi Yao <yaow89@gmail.com>
2012-04-18 16:55:19 +0100

    Background stuff works

--- run.m
+++ run.m
@@ -1,26 +1,22 @@
-function run()
-
 files = dir(fullfile('Images', '*.mat'));
 field = imread('Images/field.jpg');
+%field = permute(field, [2 1 3]);
 
 % Clockwise from top left.
 %rect = [ 130, 41; 429, 40; 452, 477; 91, 478 ];
 %rect = [ 41, 130; 40, 429; 477, 452; 478, 91 ];
 
-rect  = [ 41, 130; 478, 91; 477, 452; 40, 429 ];
+rect  = [ 41, 130; 477, 91; 474, 452; 40, 429 ];
 
 % If we want to redo these... 
 find_new_points = 0; 
 
 % First pick out the trapezoid
 test_im = find_trapezoid(480, 640, rect);
-%test_im = test_im';
 
 % Find the homographic transfer
-field_x = size(field, 1);
-field_y = size(field, 2);
-
-%UV = [[41,130]',[478,91]',[477,452]',[40,429]']';    % target points
+field_x = size(field, 2);
+field_y = size(field, 1);
 
 UV = [[41, 130]', [40, 429]', [478, 91]', [477, 452]']'; 
 XY = [[1,1]',[1,field_x]',[field_y,1]',[field_y,field_x]']';    % source points
@@ -35,6 +31,7 @@ avg_z  = zeros(480, 640, n_files);
 
 % Preload all the images
 for i = 1 : n_files,
+
     curr_image = load(fullfile('Images',files(i).name));
     
     vars = fieldnames(curr_image);
@@ -48,7 +45,6 @@ for i = 1 : n_files,
     
     % Swap dimensions 1 and 2
     final = permute(im, [2 1 3]);
-    
     
     images{i} = final;
     avg_z(:,:,i) = final(:,:,3);
@@ -64,16 +60,34 @@ for i = 1 : n_files,
 end
 
 %% Separate the man from the wall.
-clear 'xyzrgb_*'
+clear 'xyzrgb_*';
+
 %% First, use the average z-coords of each pixel to decide where the wall is
-mean_z = mean(avg_z, 3);
-std_z  = std(avg_z, 0, 3);
+new_avg_z = avg_z(:,:,1:7);
+
+mean_z = mean(new_avg_z, 3);
+
+variance_z = (avg_z - repmat(mean_z, [1,1,n_files])).^2;
+
+std_z  = mean(variance_z, 3);
+[I,J] = find(std_z == 0);
+
+[~,~,v] = find(std_z);
+new_std = mean(v);
+
+disp(['Number of pixels with standard deviation 0: ' num2str(length(I))]);
+disp(['New standard deviation for these pixels: ' num2str(new_std)]);
+
+for i = 1 : length(I),
+    if (I(i) < 240),
+        std_z(I(i), J(i)) = new_std;
+    end
+end
 
 %% Preload all the homography
 
 [I, J] = find(test_im == 1);
 
-%%
 test_im_2 = zeros(480, 680, 3);
 for i = 1 : length(I),
    v = P * [ I(i), J(i), 1 ]';        % project destination pixel into source
@@ -83,35 +97,58 @@ for i = 1 : length(I),
    if x == 0, x = 1; end
    if y > field_y, y = field_y; end
    if x > field_x, x = field_x; end
-   test_im_2(I(i), J(i),:) = field(x,y,:);
+   test_im_2(I(i), J(i),:) = field(y,x,:);
 end
 
 %%
 
 for i = 1 : n_files,
-
+    
     final = images{i};
     final_z = final(:,:,3);
     std_z = std_z .* test_im;
-    [I,J] = find(abs(final_z - mean_z) < std_z);
+
+    is_background = abs(final_z - mean_z) < std_z;
     
-    for i = 1 : length(I),  
-        final(I(i),J(i),4:6) = test_im_2(I(i),J(i),:);   % transfer colour
+    converged = 0;
+    [I,J] = find(~is_background);
+    old_length = length(I);
+    disp(num2str(old_length));    
+    
+    while (~converged),
+        
+        for j = 1 : length(I),
+            pI = I(j); pJ = J(j);
+            
+            if test_im(pI, pJ) == 1,
+                neighbours = is_background(pI - 1: pI + 1, pJ - 1 : pJ + 1);
+                
+                if (sum(sum(neighbours)) > 6), is_background(pI, pJ) = 1; end
+            end
+        end
+        
+        [I,J] = find(~is_background);
+        new_length = length(I);
+        
+        if (old_length == new_length), 
+            converged = 1; 
+        end
+        
+        disp(num2str(new_length));
+        old_length = new_length;
     end
+    disp('Converged');
     
+    [I,J] = find(is_background);
     
-%     for r = 1 : size(final,1),
-%         for c = 1 : size(final,2),
-%             if (test_im(r,c) == 1),
-%                if ( abs(final(r,c,3) - mean_z(r,c)) < std_z(r,c) ),
-%                    final(r,c,4:6)=test_im_2(r,c,:);   % transfer colour
-%                end
-%             end
-%         end
-%     end
+    for j = 1 : length(I),
+        final(I(j),J(j),4:6) = test_im_2(I(j),J(j),:);   % transfer colour
+    end
+
     % RGB image layers must be converted to uint8 to display
     imshow(uint8(final(:,:,4:6)));
     pause;
 
 end    
-
+disp('Done');
+close all;
